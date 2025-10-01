7885e7c5d0774e4d147e03ba69efe5ba583f9fce
Your Name <you@example.com>
2025-10-01 22:35:14 +0530

    first push SQL

---
+++ oct_Day1.sql
@@ -0,0 +1,70 @@
+#30 Days of SQL - From Basic to Advanced Level
+
+1.What is SQL?
+
+👉 SQL (Structured Query Language) is a language used to communicate with databases.
+It is used to store, retrieve, manage, and manipulate data in a relational database.
+
+2.How Does SQL Work?
+
+👉We interact with databases using SQL queries. DBMS tools like MySQL and SQL Server have their own SQL engine and an interface 
+where users can write and execute SQL queries.
+
+3.What is data?
+
+👉 Data is raw facts or details we collect, like numbers, words, or symbols.
+
+#Types of Data:
+.Structured → Organized neatly in tables.
+
+.Unstructured → Free-form like images or text.
+
+.Semi-Structured → In-between, like JSON/XML.
+
+4.What is a Database?
+
+👉 A database is an organized collection of data that is stored and managed so it can be easily accessed, updated, and retrieved.
+
+5.What is DBMS?
+
+👉 DBMS (Database Management System) is software that helps us create, store, manage, and retrieve data from a database.ALTER 
+
+6.What is RDBMS?
+
+👉 RDBMS (Relational Database Management System) is a type of DBMS where data is stored in tables (rows & columns)
+and relationships are maintained between them using keys.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+SQL Basics
+
+
+Comments
+
+Single-line: -- comment
+Multi-line: /* comment */
+
+
+
+
+
+
+
+
+
+
+
